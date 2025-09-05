4ee7b09b7dbe78d340032eb100c74b0656e1e466
Harsh Mishra <148937385+harshmishra00@users.noreply.github.com>
2025-09-06 02:07:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Heap Sort/heap-sort.cpp
@@ -0,0 +1,11 @@
+// The functions should be written in a way that array become sorted
+// in increasing order when heapSort() is called.
+
+class Solution {
+  public:
+    // Function to sort an array using Heap Sort.
+    void heapSort(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        
+    }
+};
