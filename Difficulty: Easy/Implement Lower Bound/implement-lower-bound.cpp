37134137ae4bebdeebc8517c024caa6219e22f4c
Harsh Mishra <148937385+harshmishra00@users.noreply.github.com>
2026-07-26 23:33:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Lower Bound/implement-lower-bound.cpp
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.cpp
@@ -2,19 +2,22 @@ class Solution {
   public:
     int lowerBound(vector<int>& arr, int target) {
         int n=arr.size();
-        int left=0;
-        int right=n-1;
-        int ans=arr.size();
+        int left=0;int right=n-1;
+        int ans=n;
+        
         while(left<=right){
             int mid=left+(right-left)/2;
             
             if(arr[mid]>=target){
                 ans=mid;
                 right=mid-1;
-            }else{
+            }
+            else if(arr[mid]<target){
                 left=mid+1;
             }
+                
         }
         return ans;
+        
     }
 };
